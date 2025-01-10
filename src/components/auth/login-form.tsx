1af9a9d1338825f0891e0f21526e60b5c6828d8c
matinsekhavat <matinhyphen@gmail.com>
2025-01-10 16:34:03 +0330

    feat: handle demo ui for form submission in Login-form

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,15 +1,61 @@
+"use client";
 import React from "react";
 import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/ui/Form";
+import { z } from "zod";
+import { LoginSchema } from "@/schema";
+import { Input } from "@/ui/Input";
 
 export default function LoginForm() {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <CardWrapper
-      headerLabel="welcome Back!"
+      headerLabel="Welcome Back!"
       backButtonLabel="Don't have an account?"
       backButtonHref="/auth/register"
       showSocial
     >
-      tet
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((values) => {
+            console.log(values.email);
+          })}
+          className="space-y-6"
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="ex: matin@example.com" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
     </CardWrapper>
   );
 }
