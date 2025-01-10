458bc6b97b1433ef3cf5bfc72222d8c62851dbd9
matinsekhavat <matinhyphen@gmail.com>
2025-01-10 16:54:49 +0330

    refactor: better zod validation error message for LoginSchema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(), // for login dont hint to hackers
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  password: z.string().min(1, {
+    message: "password  is required",
+  }), // for login dont hint to hackers
 });
