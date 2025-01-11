926ce93e1abc73e5ea56301cf6cae2a82f8a40f6
matinsekhavat <matinhyphen@gmail.com>
2025-01-11 21:28:11 +0330

    feat: implement register action and zod schema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -6,3 +6,11 @@ export const LoginSchema = z.object({
     message: "password  is required",
   }), // for login dont hint to hackers
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  password: z.string().min(6, {
+    message: "minimum 6 characters is requires",
+  }),
+  name: z.string().min(1, { message: "name is required" }),
+});
