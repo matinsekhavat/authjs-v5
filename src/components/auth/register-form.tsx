b03b68da8d5b0a11e562f940c745391a6d315008
matinsekhavat <matinhyphen@gmail.com>
2025-01-11 21:30:40 +0330

    feat: implement register login form content with demo functionality

---
+++ src/components/auth/register-form.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState, useTransition } from "react";
+import { CardWrapper } from "./card-wrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/ui/Form";
+import { z } from "zod";
+import { RegisterSchema } from "@/schema";
+import { Input } from "@/ui/Input";
+import { Button } from "@/ui/Button";
+// import { FormError } from "../form-error";
+// import { FormSuccess } from "../form-success";
+import { login } from "@/actions/login";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
+export default function RegisterForm() {
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string>("");
+  const [success, setSuccess] = useState<string>("");
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof RegisterSchema>) => {
+    setError("");
+    setSuccess("");
+    startTransition(async () => {
+      const res = await login(values);
+      setError(res?.error as string);
+      setSuccess(res?.success as string);
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Create an Account"
+      backButtonLabel="Already have an acount?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="ex: matin"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="ex: matin@example.com"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter your password"
+                      disabled={isPending}
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            submit
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
