583186d8d16933feb165eef436d54ea0dbdda062
matinsekhavat <matinhyphen@gmail.com>
2025-01-15 14:35:09 +0330

    feat: enhance login functionality with error handling and session management

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -1,13 +1,33 @@
 "use server";
 
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema } from "@/schema";
+import { signIn } from "@/utils/auth";
+import { AuthError } from "next-auth";
+
 import { z } from "zod";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
-  if (!validatedFields) {
+  if (!validatedFields || !validatedFields.success) {
     return { error: "Invalid Fields" };
   }
-  console.log(values);
+  const { email, password } = validatedFields.data;
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "invalid credentials" };
+        default:
+          return { error: "sth went wrong" };
+      }
+    }
+  }
   return { success: "Email Sent!" };
 };
--- src/utils/auth.config.ts
+++ src/utils/auth.config.ts
@@ -1,10 +1,31 @@
 // cause prisma by default not support edge runtime, so we need to use this file to config the auth middleware
 
 // we use this file to config the auth middleware
-import Github from "next-auth/providers/github";
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "@/_data/user";
+import { LoginSchema } from "@/schema";
 import type { NextAuthConfig } from "next-auth";
-
+import Credentials from "next-auth/providers/credentials";
 
 export default {
-  providers: [Github],
+  providers: [
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (!validatedFields.success) {
+          throw new Error("credenetials are not valid");
+        }
+        const { email, password } = validatedFields.data;
+        const user = await getUserByEmail(email);
+        if (!user || !user.password) {
+          return null;
+        }
+        const passwordMatches = await bcrypt.compare(password, user.password);
+        if (passwordMatches) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
